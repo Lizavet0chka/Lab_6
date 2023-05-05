0fc6d53d6dcd1e693893931c4394a647b53fbb80
Liza Dakhno <lizadakhno@gmail.com>
2023-05-05 23:36:31 +0200

    Datei in txt file gespeichert

---
+++ Lab_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lab_6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Train[] trains = new Train[1];
+            for (int i = 0; i < trains.Length; i++)
+            {
+                trains[i]._ziel = Console.ReadLine();
+                trains[i]._nummer = Convert.ToInt32(Console.ReadLine());
+                trains[i]._abfahrt = Console.ReadLine();
+            }
+            SaveInTxtFile(trains);
+        }
+        public static void SaveInTxtFile(Train[] trains)
+        {
+            FileStream fileName = File.Open(@"trains.txt", FileMode.Open);
+            using (var StringWriter = new StreamWriter(fileName))
+                {
+                    foreach (Train train in trains)
+                    {
+                        StringWriter.WriteLine($"{train._ziel} {train._nummer} {train._abfahrt}");
+                    }
+                }
+
+        }
+    }
+}
---
+++ Lab_6/Train.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Xml.Serialization;
+
+namespace Lab_6
+{
+    [XmlRoot("train")]
+    public struct Train
+    {
+        [XmlElement] 
+        public string _ziel;
+        [XmlElement]
+        public int _nummer;
+        [XmlElement]
+        public string _abfahrt;
+        //public Train(string str)
+        //{
+        //    string[] train = Regex.Split(str,@"\s+");
+        //    _ziel = train[0];
+        //    _nummer = Convert.ToInt32(train[1]);
+        //    _abfahrt = train[2];
+        //}
+        public override string ToString()
+        {
+            return $"{_ziel} {_nummer} {_abfahrt}";
+        }
+    }
+}
